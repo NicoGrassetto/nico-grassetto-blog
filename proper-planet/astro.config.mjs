749c8ccf5c5206009906bbdaf15d858c5f60d10d
Nico Grassetto <nicograssetto@gmail.com>
2025-06-08 14:38:13 +0200

    Initial commit: Astro blog with GitHub Pages deployment

--- proper-planet/astro.config.mjs
+++ proper-planet/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ngrassetto.github.io',
+  base: '/nico-grassetto-blog',
+});
