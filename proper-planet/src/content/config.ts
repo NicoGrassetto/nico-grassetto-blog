49046f4d7bcfd4ecf8b05426680d8dd00f851e19
Nico Grassetto <nicograssetto@gmail.com>
2025-06-08 16:55:29 +0200

    Remove artifacts collection and related components from the project

--- proper-planet/src/content/config.ts
+++ proper-planet/src/content/config.ts
@@ -10,18 +10,6 @@ const blog = defineCollection({
   }),
 });
 
-const artifacts = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    description: z.string(),
-    demo: z.string().optional(),
-    github: z.string().optional(),
-  }),
-});
-
 export const collections = {
   blog,
-  artifacts,
 };
